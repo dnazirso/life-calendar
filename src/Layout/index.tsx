a648ebac3f33ab9a01eea752e48d394ac5d529fe
d-nazir <darryl@ctrlup.io>
2022-01-26 16:52:25 +0200

    feat: 🎸 Login form

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -3,7 +3,13 @@ import { CssBaseline, Grid } from "@mui/material";
 import Header from "./Header";
 import { Footer } from "./Footer";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+  children,
+  signed,
+}: {
+  children: ReactNode;
+  signed?: boolean;
+}) {
   return (
     <Grid container direction="column" minHeight="100vh">
       <CssBaseline />
@@ -13,9 +19,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <Grid item container justifyContent="center" alignItems="center" xs>
         <Grid item>{children}</Grid>
       </Grid>
-      <Grid item>
-        <Footer />
-      </Grid>
+      <Grid item>{signed && <Footer />}</Grid>
     </Grid>
   );
 }
--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,5 +1,37 @@
+import { Button, Stack, TextField } from "@mui/material";
 import Layout from "../Layout";
 
 export default function Login() {
-  return <Layout>Login</Layout>;
+  return (
+    <Layout>
+      <Stack component="form" noValidate spacing={3}>
+        <TextField
+          placeholder="Name"
+          label="Name"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <TextField
+          placeholder="E-mail"
+          type="email"
+          label="E-mail"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <TextField
+          id="date"
+          type="date"
+          label="Birthdate"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <Button variant="outlined" sx={{ py: 1.75 }}>
+          Login
+        </Button>
+      </Stack>
+    </Layout>
+  );
 }
